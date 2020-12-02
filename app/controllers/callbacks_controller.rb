6d6963916f85977b2e52cfcda6586555c6d33216
TravisSLee <travisslee223@gmail.com>
2020-12-02 11:54:51 -0500

    add omniauth

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,7 @@
+class CallbacksController < Devise::OmniAithController
+    def github
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        sign_in_and_redirect @user
+    end
+end
