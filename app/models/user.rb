e1c45eea50a7130d86be969bf93fd4d142c6568c
TravisSLee <travisslee223@gmail.com>
2020-12-02 12:12:02 -0500

    test push

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provide = auth.provide
+      user.username = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.email
+      user.password = Devise.friendly_token[0...20]  
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations' }
+  devise_for :users, :controllers => {registrations: 'registrations', :omni_auth_callbacks => 'callbacks' }
   root to: 'application#welcome'
+
+  devise_scope :usser do
+    get 'login', to: 'devise/sessions#new', as: 'login' 
+  end
 end
