a7fbff604739bb394d2cdbee918f9e4ee7935fd1
TravisSLee <travisslee223@gmail.com>
2020-12-03 01:15:30 -0500

    update omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
 
   def self.from_omniauth(auth)
-    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.username = auth.info.name
